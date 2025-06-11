5e633169a569e2977e6ca92f499d43e9fe720b94
JanoschHagenbeck <janosch.hagenbeck@gmail.com>
2025-06-11 16:48:46 +0000

    Added skipping of empty or corrupted files

--- data_helpers.py
+++ data_helpers.py
@@ -16,23 +16,37 @@ def load_preprocessed_samples(data_dir: str) -> Tuple[List[np.ndarray], List[int
     """
     all_samples: List[np.ndarray] = []
     all_labels: List[int] = []
+    amount_empty_or_corrupted_files: int = 0
 
     # Iterate through all .pkl files in the given directory
     for filename in os.listdir(data_dir):
         if filename.endswith("_preprocessed.pkl"):
             filepath = os.path.join(data_dir, filename)
+            
+            # Skip empty files
+            if os.path.getsize(filepath) == 0:
+                print(f"Skipped empty file: {filename}")
+                amount_empty_or_corrupted_files += 1
+                continue
 
-            with open(filepath, 'rb') as file:
-                data = pickle.load(file)
+            try:
+                with open(filepath, 'rb') as file:
+                    data = pickle.load(file)
 
-                if not data or "channels" not in data:
-                    continue
+                    if not data or "channels" not in data:
+                        continue
 
-                # Iterate through each ECG channel
-                for channel_data in data["channels"]:
-                    windows = channel_data.get("windows", [])
-                    labels = channel_data.get("labels", [])
-                    all_samples.extend(windows)
-                    all_labels.extend(labels)
-
+                    # Iterate through each ECG channel
+                    for channel_data in data["channels"]:
+                        windows = channel_data.get("windows", [])
+                        labels = channel_data.get("labels", [])
+                        all_samples.extend(windows)
+                        all_labels.extend(labels)
+            
+            except (EOFError, pickle.UnpicklingError) as e:
+                # print(f"Warning: {filename} is empty or corrupted.")
+                print(f"Corrupted pickle file: {filename} ({e})")
+                amount_empty_or_corrupted_files += 1
+                continue
+    print(f"Amount empty or corrupted files {amount_empty_or_corrupted_files}.")
     return all_samples, all_labels
